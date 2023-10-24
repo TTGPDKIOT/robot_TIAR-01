620da38b11e465ffee600b42c0d418f0f38fbb6b
tnlong2203 <141475166+tnlong2203@users.noreply.github.com>
2023-10-24 15:22:03 +0700

    Update Test_ThreeMBDV_Motor.py
    Modify to continuously input the velocity

--- Test_TIAR-01/MBDV_motor_test/Test_ThreeMBDV_Motor.py
+++ Test_TIAR-01/MBDV_motor_test/Test_ThreeMBDV_Motor.py
@@ -1,17 +1,16 @@
 from pymodbus.client import ModbusSerialClient
-import time
 
 def decimal_to_hex(input_value):
-        hex_value = hex(input_value & 0xFFFF).upper()
-        if len(hex_value) < 6:
-            hex_value = '0x' + '0' * (6 - len(hex_value)) + hex_value[2:]
-        return hex_value
+    hex_value = hex(input_value & 0xFFFF).upper()
+    if len(hex_value) < 6:
+        hex_value = '0x' + '0' * (6 - len(hex_value)) + hex_value[2:]
+    return hex_value
 
 def main():
     # Connect to Modbus device
     client = ModbusSerialClient(
         method="rtu",
-        port="COM7",
+        port="/dev/ttyUSB0",
         baudrate=115200,
         stopbits=1,
         parity="N",
@@ -19,49 +18,36 @@ def main():
     )
     if not client.connect():
         print("Failed to connect to Modbus device")
-        
         return
 
     print("Connected to Modbus device")
-    client.write_register(0x7C, 0x9F, unit=0x01)            #ME
-    # client.write_register(0x0152, 0x0258, unit=0x01)        #JA = 600
-    # client.write_register(0x0154, 0x0258, unit=0x01)        #JL = 600
-    client.write_register(0x7C, 0x9F, unit=0x02)            #ME
-    # client.write_register(0x0152, 0x0258, unit=0x02)        #JA = 600
-    # client.write_register(0x0154, 0x0258, unit=0x02)        #JL = 600
-    client.write_register(0x7C, 0x9F, unit=0x0)            #ME
-    # client.write_register(0x0152, 0x0258, unit=0x03)        #JA = 600
-    # client.write_register(0x0154, 0x0258, unit=0x03)        #JL = 600
-    input_value = int(input("Velocity: "))
-    hex_value = decimal_to_hex(input_value)
+    client.write_registers(342, [0x0000, 0], unit=0x01)
+    client.write_register(0x7C, 0x96, unit=0x01)  # CJ
+    client.write_registers(342, [0x0000, 0], unit=0x02)
+    client.write_register(0x7C, 0x96, unit=0x02)  # CJ
+    client.write_registers(342, [0x0000, 0], unit=0x03)
+    client.write_register(0x7C, 0x96, unit=0x03)  # CJ
     try:
-        if input_value >= 0:
-            print("motor clockwise")
-            client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x01)
-            client.write_register(0x7C, 0x96, unit=0x01)         #CJ
-            client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x02)
-            client.write_register(0x7C, 0x96, unit=0x02)
-            client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x03)
-            client.write_register(0x7C, 0x96, unit=0x03)
-            time.sleep(5)
-            client.write_register(0x7C, 0xD8, unit=0x01)
-            client.write_register(0x7C, 0xD8, unit=0x02)
-            client.write_register(0x7C, 0xD8, unit=0x03)         #SM
-        else:
-            print("motor anticlockwise")
-            client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x01)
-            client.write_register(0x7C, 0x96, unit=0x01)         #CJ
-            client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x02)
-            client.write_register(0x7C, 0x96, unit=0x02)         #CJ
-            client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x03)
-            client.write_register(0x7C, 0x96, unit=0x03)         #CJ
-            time.sleep(5)
-            client.write_register(0x7C, 0xD8, unit=0x01)         #SM
-            client.write_register(0x7C, 0xD8, unit=0x02)         #SM
-            client.write_register(0x7C, 0xD8, unit=0x03)         #SM
+        while True:
+            input_value = int(input("Velocity: "))
+            hex_value = decimal_to_hex(input_value)
+
+            if input_value >= 0:
+                print("motor clockwise")
+                client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x01)
+                client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x02)
+                client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x03)
+            else:
+                print("motor anticlockwise")
+                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x01)
+                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x02)
+                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x03)
+                
+        
     except Exception as e:
         print(f"An error occurred: {e}")
 
+    client.close()
+
 if __name__ == '__main__':
     main()
-
