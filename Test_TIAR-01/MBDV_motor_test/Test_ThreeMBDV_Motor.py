9233498fd8ad231779e4971041c1d186e65775b8
tnlong2203 <141475166+tnlong2203@users.noreply.github.com>
2023-10-24 15:47:07 +0700

    Update Test_ThreeMBDV_Motor.py
    Update code

--- Test_TIAR-01/MBDV_motor_test/Test_ThreeMBDV_Motor.py
+++ Test_TIAR-01/MBDV_motor_test/Test_ThreeMBDV_Motor.py
@@ -16,36 +16,51 @@ def main():
         parity="N",
         timeout=0.2
     )
+    
     if not client.connect():
         print("Failed to connect to Modbus device")
         return
-
     print("Connected to Modbus device")
-    client.write_registers(342, [0x0000, 0], unit=0x01)
-    client.write_register(0x7C, 0x96, unit=0x01)  # CJ
-    client.write_registers(342, [0x0000, 0], unit=0x02)
-    client.write_register(0x7C, 0x96, unit=0x02)  # CJ
-    client.write_registers(342, [0x0000, 0], unit=0x03)
-    client.write_register(0x7C, 0x96, unit=0x03)  # CJ
+
+    client.write_registers(342, [0x0000, 0x0000], unit=0x01)
+    client.write_registers(342, [0x0000, 0x0000], unit=0x02)
+    client.write_registers(342, [0x0000, 0x0000], unit=0x03)
+    client.write_register(0x7C, 0x96, unit=0x01)
+    client.write_register(0x7C, 0x96, unit=0x02)
+    client.write_register(0x7C, 0x96, unit=0x03)
+    print("Motor ready!")
+
     try:
         while True:
             input_value = int(input("Velocity: "))
             hex_value = decimal_to_hex(input_value)
 
-            if input_value >= 0:
-                print("motor clockwise")
-                client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x01)
+            if input_value > 0:
+                print("Motor clockwise!")
+                client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x01)        
                 client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x02)
                 client.write_registers(342, [0x0000, int(hex_value, 16)], unit=0x03)
-            else:
-                print("motor anticlockwise")
+            if input_value < 0:
+                print("Motor anticlockwise!")
                 client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x01)
-                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x02)
-                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x03)
+                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x02)       
+                client.write_registers(342, [0xFFFF, int(hex_value, 16)], unit=0x03)      
+            if input_value == 0:
+                print("Motor stop!")
+                client.write_registers(342, [0x0000, 0x0000], unit=0x01)
+                client.write_registers(342, [0x0000, 0x0000], unit=0x02)
+                client.write_registers(342, [0x0000, 0x0000], unit=0x03)
                 
-        
     except Exception as e:
         print(f"An error occurred: {e}")
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt: Turn off the motor!")
+        client.write_registers(342, [0x0000, 0x0000], unit=0x01)
+        client.write_registers(342, [0x0000, 0x0000], unit=0x02)
+        client.write_registers(342, [0x0000, 0x0000], unit=0x03)
+        client.write_register(0x7C, 0xD8, unit=0x01)
+        client.write_register(0x7C, 0xD8, unit=0x02)
+        client.write_register(0x7C, 0xD8, unit=0x03)
 
     client.close()
 
