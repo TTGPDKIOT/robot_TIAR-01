96d22707405ca18aa8c3f53b67c8cf88644918da
SangPhamCV <147231602+SangPhamCV@users.noreply.github.com>
2023-11-28 10:37:43 +0700

    Update odometry.py

--- control_motor/odometry.py
+++ control_motor/odometry.py
@@ -2,35 +2,59 @@
 
 import rospy
 import tf
-from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from math import cos, sin, pi
+from math import *
+from std_msgs.msg import Int64MultiArray
 
-def velCallback(msg):
-    global vel
-    vel = msg
+# Constants
+ENCODER_PER_METER = 15915
 
-def updateOmniFake(tf_broadcaster):
-    global prev_update_time
+# Global variables
+pre_encoder = [0, 0, 0]
+current_encoder = [0, 0, 0]
+distance = [0, 0, 0]
+
+odom = Odometry()
+odom_pose = [0, 0, 0]
+odom_vel = [0, 0, 0]
+wheel_vel = [0, 0, 0]
+prev_update_time = rospy.Time()  # Define prev_update_time
+
+def encoderCallback(data):
+    global pre_encoder, current_encoder, distance, prev_update_time, odom_vel, odom
+
     time_now = rospy.Time.now()
     step_time = time_now - prev_update_time
     prev_update_time = time_now
 
-    updateOdometry(step_time)
+    encoder_data = data.data  # Define encoder_data here
+
+    for i in range(3):
+        if encoder_data[i] != 0 and pre_encoder[i] != 0:
+            current_encoder[i] = encoder_data[i] - pre_encoder[i]
+            distance[i] = current_encoder[i] / ENCODER_PER_METER
+        pre_encoder[i] = encoder_data[i]
+
+    step_time_sec = step_time.to_sec()  # Convert the rospy.Duration to seconds
+
+    # Update the velocities and positions based on the encoder data
+    wheel_vel[0] = distance[0] / step_time_sec
+    wheel_vel[1] = distance[1] / step_time_sec
+    wheel_vel[2] = distance[2] / step_time_sec
+
+    odom_vel[0] =(-1/sqrt(3)) * wheel_vel[0] + 0 * wheel_vel[1] + (1/sqrt(3)) * wheel_vel[2]
+    odom_vel[1] =(1/3) * wheel_vel[0] + (-2/3) * wheel_vel[1] + (1/3) * wheel_vel[2]
+    odom_vel[2] =(1/0.78) * wheel_vel[0] + (1/0.78) * wheel_vel[1] + (1/0.78) * wheel_vel[2]
+
+    updateOdometry(step_time_sec)
     odom.header.stamp = time_now
     pub_odom.publish(odom)
 
+    tf_broadcaster = tf.TransformBroadcaster()
     updateTF(tf_broadcaster, time_now)
 
-def updateOdometry(diff_time):
-    dt = diff_time.to_sec()
-
-    # Robot state - velocity
-    odom_vel[0] = vel.linear.x
-    odom_vel[1] = vel.linear.y
-    odom_vel[2] = vel.angular.z
-
-    # Robot state - position (Euler Discretize = Runge-Kutta 1st order)
+def updateOdometry(dt):
+    # Update the robot's position and orientation
     odom_pose[2] += odom_vel[2] * dt
     if odom_pose[2] > pi:
         odom_pose[2] -= 2 * pi
@@ -40,7 +64,7 @@ def updateOdometry(diff_time):
     odom_pose[0] += (odom_vel[0] * cos(odom_pose[2]) - odom_vel[1] * sin(odom_pose[2])) * dt
     odom_pose[1] += (odom_vel[0] * sin(odom_pose[2]) + odom_vel[1] * cos(odom_pose[2])) * dt
 
-    # Update Odometry
+    # Update the Odometry message
     odom.pose.pose.position.x = odom_pose[0]
     odom.pose.pose.position.y = odom_pose[1]
     odom.pose.pose.position.z = 0.0
@@ -65,10 +89,9 @@ def updateTF(tf_broadcaster, time_now):
 
 def initialize_node():
     rospy.init_node("omni_fake_node")
-    global vel, odom, odom_pose, odom_vel, prev_update_time, frame_id, child_frame_id
+    global odom, odom_pose, odom_vel, prev_update_time, frame_id, child_frame_id
 
     prev_update_time = rospy.Time()
-    vel = Twist()
     odom = Odometry()
     odom_pose = [0, 0, 0]
     odom_vel = [0, 0, 0]
@@ -85,15 +108,11 @@ def initialize_node():
 
 if __name__ == '__main__':
     initialize_node()
-    sub_vel = rospy.Subscriber("/cmd_vel", Twist, velCallback)
+    sub_vel = rospy.Subscriber("/encoder_pub", Int64MultiArray, encoderCallback)
     pub_odom = rospy.Publisher("/odom", Odometry, queue_size=10)
-    tf_broadcaster = tf.TransformBroadcaster()
 
-    r = rospy.Rate(10)
+    r = rospy.Rate(100)
     rospy.loginfo("Initial Omni Robot Fake !!!")
-    
-    while not rospy.is_shutdown():
-        updateOmniFake(tf_broadcaster)
-        r.sleep()
 
-    rospy.spin()
+    while not rospy.is_shutdown():
+        r.sleep()
